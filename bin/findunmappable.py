58e66cf217fc21729a270d67f3117fc058817d86
@nikitin-p <niki4pingvi@mail.ru>
2023-03-14 16:59:50 +0300

    Fixed indexing in findunmappable.py

--- bin/findunmappable.py
+++ bin/findunmappable.py
@@ -55,14 +55,18 @@ def array_to_bed(arr):
 name_of_region = str(sys.argv[1]).split(".")[1]
 
 test_mul = pd.read_csv(sys.argv[1], sep='\t', encoding='utf-8', engine='python')
-test_mul = test_mul.iloc[56:]
-test_mul_arr = test_mul[str(test_mul.columns[0])]
+
 test_mur = pd.read_csv(sys.argv[2], sep='\t', encoding='utf-8', engine='python')
-test_mur = test_mul.iloc[:-56]
-test_mur_arr = test_mur[str(test_mur.columns[0])]
 
 chr_name = list(test_mul.columns.values)[0].split(' ')[1].split('=')[1]
+
 spacing = int(list(test_mur.columns.values)[0].split(' ')[2].split('=')[1])
+
+test_mul = test_mul.iloc[56:].reset_index()
+test_mul_arr = test_mul[str(test_mul.columns[0])]
+
+test_mur = test_mur.iloc[:-56]
+test_mur_arr = test_mur[str(test_mur.columns[0])]
 
 #start = 56
 min_read_length = 150
